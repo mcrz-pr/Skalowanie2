074cb3cc7e2b19ee68d7cdcc007568cca64ee285
student-14 <terleckawika123@gmail.com>
2022-10-17 13:30:09 +0200

    DRUGA CZĘŚĆ LAYOUTU I POCZĄTEK DZIAŁANIA

--- app/src/main/java/com/example/skalowanie/MainActivity.java
+++ app/src/main/java/com/example/skalowanie/MainActivity.java
@@ -3,12 +3,22 @@ package com.example.skalowanie;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+
+    ImageView zdj;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        zdj = (ImageView) findViewById(R.id.imageView);
+    }
+
+    public void klik(View view) {
+        ImageView zdj = new ImageView(this);
+        zdj.setImageResource(R.drawable.ponczek);
     }
 }
