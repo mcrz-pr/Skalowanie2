78c79d91edbb9bea90646d55b1fb9fe6075836d7
Dominos0321 <dsch792@gmail.com>
2022-10-24 11:09:19 +0200

    Uskalowanie ImageView i layoutu i zdefiniowanie seekbarów

--- app/src/main/java/com/example/skalowanie/MainActivity.java
+++ app/src/main/java/com/example/skalowanie/MainActivity.java
@@ -5,20 +5,23 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.SeekBar;
 
 public class MainActivity extends AppCompatActivity {
 
     ImageView zdj;
+    SeekBar r,g,b;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         zdj = (ImageView) findViewById(R.id.imageView);
+        r = (SeekBar) findViewById(R.id.seekBar2);
+        g = (SeekBar) findViewById(R.id.seekBar3);
+        b = (SeekBar) findViewById(R.id.seekBar4);
     }
-
     public void klik(View view) {
-        ImageView zdj = new ImageView(this);
         zdj.setImageResource(R.drawable.ponczek);
     }
 }
